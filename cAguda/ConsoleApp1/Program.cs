1882eceefd1b0c323440b488bdb8f28bd4d333dd
Noxorivm <80338826+Noxorivm@users.noreply.github.com>
2022-11-22 19:00:48 +0100

    Update Program.cs

--- cAguda/ConsoleApp1/Program.cs
+++ cAguda/ConsoleApp1/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+namespace Conversion
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            //Conversión explícita
+            double miDouble = 13.37;
+            int miInt;
+
+            miInt = (int)miDouble;
+
+            //Conversión implicita 
+
+            int num = 123456;
+            long numg = num;
+
+            float miFloat = 13,37F;
+            double miNuevoDouble = miFloat;
+
+            //Conversión de tipo
+            string miString = miDouble.ToString();
+
+            Console.WriteLine(miString);
+            Console.Read();
+
         }
     }
 }
